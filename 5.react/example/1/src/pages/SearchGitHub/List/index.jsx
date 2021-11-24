6eba096acdcaf700b3d240ee6ba05154d24e03ee
seven <1076931904@qq.com>
2021-11-24 22:01:15 +0800

    更新

--- 5.react/example/1/src/pages/SearchGitHub/List/index.jsx
+++ 5.react/example/1/src/pages/SearchGitHub/List/index.jsx
@@ -3,14 +3,24 @@ import './index.css'
 
 export default class List extends Component {
   render() {
+    const { users, isFirst, isLoading, err } = this.props;
     return (
       <div className="row">
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" alt="avatar" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {
+          isFirst ? <h2>&ensp;欢迎使用，输入关键字，随后点击搜索</h2> :
+					isLoading ? <h2>&ensp;Loading......</h2> :
+					err ? <h2 style={{color:'red'}}>&ensp;{err}</h2> :
+          users.map(item => {
+            return (
+              <div className="card" key={item.id}>
+                <a href={item.html_url} target="_blank" rel="noreferrer">
+                  <img src={item.avatar_url} alt="avatar" style={{ width: '100px' }} />
+                </a>
+                <p className="card-text">{item.login}</p>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
--- 5.react/example/1/src/pages/SearchGitHub/Search/index.jsx
+++ 5.react/example/1/src/pages/SearchGitHub/Search/index.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+
+  search = () => {
+    const { value: keyWord } = this.keyWordNode;
+    this.props.updateSearchState({ isLoading: true, isFirst: false })
+    axios.get(`/api1/search/users?q=${keyWord}`).then(
+      response => {
+        //请求成功后通知App更新状态
+        this.props.updateSearchState({ isLoading: false, users: response.data.items })
+      },
+      error => {
+        //请求失败后通知App更新状态
+        this.props.updateSearchState({ isLoading: false, err: error.message })
+      }
+    )
+  }
+
   render() {
     return (
       <section className="jumbotron">
-        <h3 className="jumbotron-heading">Search Github Users</h3>
+        <h3 className="jumbotron-heading">搜索Github用户</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
+          <input ref={c => this.keyWordNode = c} type="text" placeholder="输入关键词点击搜索" />&nbsp;
+          <button onClick={this.search}>搜索</button>
         </div>
       </section>
     )
